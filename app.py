960b820ceea4deea41704060dd1c8ef60c4412c9
Gabriel Peres <34287414+SirAargon@users.noreply.github.com>
2019-01-30 22:24:49 -0200

    Local terminado
    Localmente esse codigo ta 100%

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask,jsonify
+from flask import request
+from flask_pymongo import PyMongo
+import json
+import requests
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+mongo = PyMongo(app)
+# ------------  Ligar server Mongo ------------------
+# cd Desktop\mongodb-win32-x86_64-2008plus-ssl-4.0.5\bin
+# mongod
+# ------------ Ligar cliente Mongo ------------------
+# cd Desktop\mongodb-win32-x86_64-2008plus-ssl-4.0.5\bin
+# mongo.exe
+#------------- Instruçoes Mongo ---------------------
+#
+@app.route('/')
+def hello_world():
+    return 'Bem Vindo A Nuvem Quebrada, por favor va para /provedor ou /cliente'
+
+
+#-----------------------------------------  1  ---------------------------------------------
+
+
+@app.route('/Provedor', methods=['POST']) #BROKER
+def cloud_Cadastrando():
+
+    received_data = json.load(request.files['datas'])
+    mongo.db.test1.insert(received_data)
+    
+    vCPU = received_data['vCPU']
+    RAM = received_data['RAM']
+    HD = received_data['HD']
+    PRECO = received_data['RS']
+    
+    msg = 'CADASTRADO COM SUCESSO:\nvCPUS : ',str(vCPU),'\nRAM :', str(RAM),'  GB\nHD :', str(HD),'  GB\nPREÇO R$ ', str(PRECO)
+    resposta = {'Mensagem':''.join(msg)}
+
+
+
+    print ('CADASTRADO COM SUCESSO:\nvCPUS : ',vCPU,'\nRAM :', RAM,'GB\nHD :', HD,'GB\nPREÇO R$', PRECO)
+    return jsonify(resposta)
+
+
+#------------------------------------------   2   --------------------------------------------------------------------
+
+
+@app.route('/cliente', methods=['POST'])
+def cloud_consultando():
+    received_data = json.load(request.files['datas'])
+    vCPU = received_data['vCPU']
+    RAM = received_data['RAM']
+    HD = received_data['HD']
+    a = mongo.db.test1.find({"$and":[ {'vCPU':{"$gte" : vCPU}},{'RAM':{"$gte" : RAM}},{'HD':{"$gte" : HD}},{'Disp': 1}]}).sort('RS').limit(1)
+    try:
+        print ('Foi Achado essa VM:\n')
+        print (a[0])
+        msg = 'Foi Achado um com as seguintes configuraçoes:\nvCPUS : ',str(a[0]['vCPU']),'\nRAM :',str(a[0]['RAM']),'  GB\nHD :',str(a[0]['HD']),'  GB\nPREÇO R$ ',str(a[0]['RS']),'\nLink de Acesso: 127.0.0.1:',str(a[0]['Prov']),'\nMaquina : ',str(a[0]['Maq'])
+        resposta = {'Mensagem':''.join(msg)} 
+    except:
+        msg ='Nenhum Recurso com essas especificações foi encontrado'
+        print(msg)
+        resposta = {'Mensagem':''.join(msg)}      
+    return jsonify(resposta)
+
+
+#-----------------------------------------------  3  ------------------------------------------------- 
+
+
+@app.route('/usa', methods=['POST'])
+def cliente_usa():
+    received_data = json.load(request.files['datas'])
+    try:
+        a = mongo.db.test1.update({'Prov':received_data['Prov'],'Maq':received_data['Maq']},{'$set' : {'Disp':0}})
+        print ('Um registro alterado')
+        msg = 'Sucesso'
+    except:
+        print('Erro, maquina inexistente')
+        msg = 'Erro'
+    return jsonify({'Mensagem': msg}) 
+    
+
+
+#--------------------------------------------   4   ---------------------------------------------------
+
+@app.route('/libera', methods=['POST'])
+def cliente_liberando():
+    received_data = json.load(request.files['datas'])
+    try:
+        a = mongo.db.test1.update({'Prov':received_data['Prov'],'Maq':received_data['Maq']},{'$set' : {'Disp':1}})
+        print ('Um registro alterado')
+        msg = 'Sucesso'
+    except:
+        print('Erro, maquina inexistente')
+        msg = 'Erro'
+    return jsonify({'Mensagem': msg}) 
+    
---
+++ cliente-provedor.py
@@ -0,0 +1,123 @@
+from flask import Flask,jsonify
+from flask import request
+from flask_pymongo import PyMongo
+import requests
+import json
+import requests
+import random
+import threading;
+
+# ------------  Ligar server Mongo ------------------
+# cd Desktop\mongodb-win32-x86_64-2008plus-ssl-4.0.5\bin
+# mongod
+# ------------ Ligar cliente Mongo ------------------
+# cd Desktop\mongodb-win32-x86_64-2008plus-ssl-4.0.5\bin
+# mongo.exe
+#------------- Instruçoes Mongo ---------------------
+#
+
+def ouvir(prov,contador):
+
+    while(1):
+        print("Digite a opção \n1-Divulgação do Provedor\n2-Consulta do Cliente\n3-Usar\n4-Liberar")
+        opc = int(input())
+
+        if opc == 1:
+            print("Digite a vCPU, RAM, HD e o Preço por hora (separado por Enters)")
+            datas = {'vCPU':int(input()), 'RAM':int(input()), 'HD':int(input()), 'RS':int(input()), 'Disp': 1, 'Prov':prov, 'Maq':contador}
+            contador = contador + 1
+
+            files = [('datas', ('datas', json.dumps(datas), 'application/json')),]
+            url = "http://127.0.0.1:5000/Provedor"
+            r = requests.post(url, files=files)
+
+            print (r.json()['Mensagem'])
+
+        if opc == 2:
+            print("Digite a vCPU, RAM e HD separado por Enters)")
+            datas = {'vCPU':int(input()), 'RAM':int(input()), 'HD':int(input())}
+
+            url = "http://127.0.0.1:5000/cliente"
+
+            files = [('datas', ('datas', json.dumps(datas), 'application/json')),]
+
+            r = requests.post(url, files=files)
+            print(r.json()['Mensagem'])
+        
+        if opc == 3:
+            print('Digite qual provedor deseja se comunicar')
+            curl = input()
+            url = 'http://127.0.0.1:',str(curl),'/usar'
+            url =''.join(url)
+            print('Maquina: ')
+            datas = {'Maq':int(input())}
+            files = [('datas', ('datas', json.dumps(datas), 'application/json')),]
+            r = requests.post(url, files=files)
+            print (r.json()['Mensagem'])
+        
+        if opc == 4:
+            print('Digite qual provedor deseja liberar')
+            curl = input()
+            url = 'http://127.0.0.1:',str(curl),'/liberar'
+            url =''.join(url)
+            print('Maquina :')
+            datas = {'Maq':int(input())}
+            files = [('datas', ('datas', json.dumps(datas), 'application/json')),]
+            r = requests.post(url, files=files)
+            print (r.json()['Mensagem'])
+
+
+
+
+
+prov = random.randint(5001,40000)
+contador = 0
+
+oi = Flask(__name__)
+
+
+@oi.route('/usar', methods=['POST'])
+def usar():
+    received_data = json.load(request.files['datas'])
+    
+    datas = {'Prov':prov, 'Maq':received_data['Maq']}
+
+    url = "http://127.0.0.1:5000/usa"
+
+    files = [('datas', ('datas', json.dumps(datas), 'application/json')),]
+
+    r = requests.post(url, files=files)
+    print(r.json()['Mensagem'])
+    if r.json()['Mensagem']=='Sucesso':
+    
+        msg = 'Obrigado por nos escolher, aproveite'
+        resposta = {'Mensagem':''.join(msg)}
+        return jsonify(resposta)
+
+@oi.route('/liberar', methods=['POST'])
+def liberar():
+    received_data = json.load(request.files['datas'])
+    
+    datas = {'Prov':prov, 'Maq':received_data['Maq']}
+
+    url = "http://127.0.0.1:5000/libera"
+
+    files = [('datas', ('datas', json.dumps(datas), 'application/json')),]
+
+    r = requests.post(url, files=files)
+    print(r.json()['Mensagem'])
+    if r.json()['Mensagem']=='Sucesso':
+    
+        msg = 'Obrigado por nos escolher, volte sempre'
+        resposta = {'Mensagem':''.join(msg)}
+        return jsonify(resposta)
+
+
+
+t_ouvir = threading.Thread(target=ouvir,args=(prov,contador));
+t_ouvir.start();
+
+if __name__=="__main__" :oi.run(host='127.0.0.1',port=prov)  
+
+
+
